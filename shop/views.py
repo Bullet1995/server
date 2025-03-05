e9c1767bc1060ee1c5c4e4d8a1924423268f2564
Bullet1995 <164767274+Bullet1995@users.noreply.github.com>
2025-03-05 23:05:01 +0300

    add: views.py

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+from shop.models import Product
+
+
+def all_products(request: HttpRequest):
+    product_titles = Product.objects.values_list('title', flat=True)
+    titles = b''
+    for title in product_titles:
+        titles += title.encode('utf-8') + b'\n'
+    return HttpResponse(titles)
+
