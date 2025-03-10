7aac640d6020c8964c4beb8a5e5034e1af0aa546
Bullet1995 <164767274+Bullet1995@users.noreply.github.com>
2025-03-10 22:00:20 +0300

    add: templates and products.html

--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
+from datetime import datetime
 from shop.models import Product
 
 
 def all_products(request: HttpRequest):
-    product_titles = Product.objects.values_list('title', flat=True)
-    titles = b''
-    for title in product_titles:
-        titles += title.encode('utf-8') + b'\n'
-    return HttpResponse(titles)
+    current_time = datetime.now(tz=None)
+    products = Product.objects.values_list('title', flat=True)
+    return render(request, 'products.html', context={'current_time': current_time, 'products':products})
+
+
 
